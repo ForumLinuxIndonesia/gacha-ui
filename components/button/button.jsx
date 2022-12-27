1f78120e1528a54347f7525a436436f19b883532
Lendra <lendradx@gmail.com>
2022-12-27 17:21:59 +0700

    Renaming from property onToggle to onClick

--- components/button/button.jsx
+++ components/button/button.jsx
@@ -12,12 +12,12 @@ function setRounded() {}
  * @color Text color
  * @variant Button Variant
  * @rounded
- * @onToggle
+ * @onClick
  * @className Your custom class
  */
 function Button(data) {
 	let classList = `${setSize(data.size)} ${setColor(data.color)} ${setVariant(data.variant)} ${setRounded(data.rounded)} ${data.className}`;
-	return <button className={classList}>{data.label}</button>;
+	return <button className={classList} onClick={data.onClick}>{data.label}</button>;
 }
 
 export default React.memo(Button);
